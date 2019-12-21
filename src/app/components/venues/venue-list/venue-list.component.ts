e9a98eead974c743dd38a1820f07f98b5e4f75f8
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-12-21 18:13:15 +0530

    Venues list working

--- src/app/components/venues/venue-list/venue-list.component.ts
+++ src/app/components/venues/venue-list/venue-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { VenuesModel } from '../venues.model';
 
 @Component({
   selector: 'app-venue-list',
@@ -6,6 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./venue-list.component.css']
 })
 export class VenueListComponent implements OnInit {
+
+  venues: VenuesModel[] = [
+    new VenuesModel(
+      'Hello',
+      'laxminagar',
+      'Delhi',
+      110092,
+      ['hello', 'there', 'how'],
+      200,
+      1000,
+      5,
+      'https://media.weddingz.in/images/b9c4d816c4c859b84e93fbaa375d1858/the-carnival-pearl-grand-alipur-delhi.jpg'
+    ),
+    new VenuesModel(
+      'Hello2',
+      'laxminagar',
+      'Delhi',
+      110092,
+      ['hello', 'there', 'how'],
+      200,
+      1000,
+      5,
+      'https://media.weddingz.in/images/b9c4d816c4c859b84e93fbaa375d1858/the-carnival-pearl-grand-alipur-delhi.jpg'
+    ),
+    new VenuesModel(
+      'Hello3',
+      'laxminagar',
+      'Delhi',
+      110092,
+      ['hello', 'there', 'how'],
+      200,
+      1000,
+      5,
+      'https://media.weddingz.in/images/b9c4d816c4c859b84e93fbaa375d1858/the-carnival-pearl-grand-alipur-delhi.jpg'
+    )
+  ];
 
   constructor() { }
 
--- src/app/components/venues/venues.model.ts
+++ src/app/components/venues/venues.model.ts
@@ -1,12 +1,12 @@
-class VenuesModel {
+export class VenuesModel {
   constructor(
     private title: string,
     private address1: string,
     private address2: string,
-    private zipCode: string,
+    private zipCode: number,
     private highlights: string[],
-    private capacity: string,
-    private price: string,
+    private capacity: number,
+    private price: number,
     private rating: number,
     private imgUrl: string
   ) { }
