dc5409ffada78aa707ab8e4a3eb8c53d12ee72c7
Avinash Anchal <you@example.comanchalavinash66@gmail.com>
2019-12-21 16:22:26 +0530

    Added some components and Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { VenuesComponent } from './components/venues/venues.component';
+import { VendorsComponent } from './components/vendors/vendors.component';
+import { GalleryWeddingsComponent } from './components/gallery-weddings/gallery-weddings.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    VenuesComponent,
+    VendorsComponent,
+    GalleryWeddingsComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/gallery-weddings/gallery-weddings.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gallery-weddings',
+  templateUrl: './gallery-weddings.component.html',
+  styleUrls: ['./gallery-weddings.component.css']
+})
+export class GalleryWeddingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
